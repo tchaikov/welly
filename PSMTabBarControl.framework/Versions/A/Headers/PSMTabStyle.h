6970eb3b125cfa4ab002ff22f6b4072eacbdadc6
Kefu Chai <tchaikov@gmail.com>
2014-11-08 13:49:34 +0800

    use PSMTabBarControl from uxp/PSMTabBarControl@bae7c721b8

--- PSMTabBarControl.framework/Versions/A/Headers/PSMTabStyle.h
+++ PSMTabBarControl.framework/Versions/A/Headers/PSMTabStyle.h
@@ -26,6 +26,8 @@
 
 @optional
 
++ (CGFloat)desiredTabBarControlHeight;
+
 // control specific parameters
 - (CGFloat)leftMarginForTabBarControl:(PSMTabBarControl *)tabBarControl;
 - (CGFloat)rightMarginForTabBarControl:(PSMTabBarControl *)tabBarControl;
@@ -38,7 +40,7 @@
 
 // cell values
 - (NSAttributedString *)attributedObjectCountStringValueForTabCell:(PSMTabBarCell *)cell;
-- (NSAttributedString *)attributedStringValueForTabCell:(PSMTabBarCell *)cell;
+- (NSAttributedString *)attributedStringValueForTabCell:(PSMTabBarCell *)cell inTabBarControl:(PSMTabBarControl *)tabBarControl;
 
 // Constraints
 - (CGFloat)minimumWidthOfTabCell:(PSMTabBarCell *)cell;
--- PSMTabBarControl.framework/Versions/A/Headers/PSMTexturedMetalTabStyle.h
+++ PSMTabBarControl.framework/Versions/A/Headers/PSMTexturedMetalTabStyle.h
@@ -1,15 +1,16 @@
 //
-//  PSMMetalTabStyle.h
+//  PSMTexturedMetalTabStyle.h
 //  PSMTabBarControl
 //
+//  Created by Christopher Cahoon on 5/8/2014.
+//  Based on PSMMetalTabStyle.h
 //  Created by John Pannell on 2/17/06.
 //  Copyright 2006 Positive Spin Media. All rights reserved.
-//
 
 #import <Cocoa/Cocoa.h>
 #import "PSMTabStyle.h"
 
-@interface PSMMetalTabStyle : NSObject <PSMTabStyle> {
+@interface PSMTexturedMetalTabStyle : NSObject <PSMTabStyle> {
 	NSImage					*metalCloseButton;
 	NSImage					*metalCloseButtonDown;
 	NSImage					*metalCloseButtonOver;
